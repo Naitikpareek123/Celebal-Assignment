791da576a4a1b34c2cfd4e3f2da0b891b585c9b0
Naitikpareek123 <112805813+Naitikpareek123@users.noreply.github.com>
2024-06-08 11:13:30 +0530

    first asignment

---
+++ tri.py
@@ -0,0 +1,22 @@
+def lowerTriangle(n):
+    for i in reversed(range(n)):
+        print("* "*(i+1))
+
+def upperTriangle(n):
+    for i in range(n):
+        print("* "*(i+1))
+
+def pyramid(n):
+    for i in range(n):
+        print(" "*(n-i)+"* "*(i+1))
+
+n=int(input("Enter the number: "))
+
+print("\nLower Triangle: ")
+lowerTriangle(n)
+
+print("\nUpper Triangle: ")
+upperTriangle(n)
+
+print("\nPyramid: ")
+pyramid(n)
